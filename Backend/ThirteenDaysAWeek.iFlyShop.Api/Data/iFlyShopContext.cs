d2ae93c5217ad67e5f37504e4526936e41dab5a2
Christopher House <chhouse@microsoft.com>
2018-12-13 10:50:39 -0600

    initial product model add

--- Backend/ThirteenDaysAWeek.iFlyShop.Api/Data/iFlyShopContext.cs
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Data/iFlyShopContext.cs
@@ -4,5 +4,11 @@ namespace ThirteenDaysAWeek.iFlyShop.Api.Data
 {
     public class iFlyShopContext : DbContext
     {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.AddFromAssembly(typeof(iFlyShopContext).Assembly);
+        }
     }
+
 }
---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ThirteenDaysAWeek.iFlyShop.Api.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
