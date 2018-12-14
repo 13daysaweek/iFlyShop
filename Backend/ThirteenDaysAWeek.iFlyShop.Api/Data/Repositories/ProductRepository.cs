6b0a5a80ea7d027aff5e1f5cb161f6592d6de1d4
Christopher House <chris.house@ecolab.com>
2018-12-14 15:37:08 -0600

    injecting cache accessor + test clean up

--- Backend/ThirteenDaysAWeek.iFlyShop.Api/Data/Repositories/ProductRepository.cs
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Data/Repositories/ProductRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Threading.Tasks;
+using ThirteenDaysAWeek.iFlyShop.Api.Caching;
 using ThirteenDaysAWeek.iFlyShop.Api.Models;
 
 namespace ThirteenDaysAWeek.iFlyShop.Api.Data.Repositories
@@ -9,10 +10,12 @@ namespace ThirteenDaysAWeek.iFlyShop.Api.Data.Repositories
     public class ProductRepository : IRepository<Product>
     {
         private readonly IContextFactory _contextFactory;
+        private readonly ICacheAccessor _cacheAccessor;
 
-        public ProductRepository(IContextFactory contextFactory)
+        public ProductRepository(IContextFactory contextFactory, ICacheAccessor cacheAccessor)
         {
             _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+            _cacheAccessor = cacheAccessor ?? throw new ArgumentNullException(nameof(cacheAccessor));
         }
 
         public async Task<IEnumerable<Product>> GetAll()
