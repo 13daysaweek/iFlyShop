ce596a8eb055dac04e32983015d067ff50fb3667
Christopher House <chris.house@ecolab.com>
2018-12-14 14:57:38 -0600

    first pass at cache accessor

---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Caching/ICacheAccessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThirteenDaysAWeek.iFlyShop.Api.Caching
+{
+    public interface ICacheAccessor
+    {
+        Task<TCacheItem> GetAsync<TCacheItem>(string cacheKey);
+
+        Task SetAsync<TCacheItem>(TCacheItem item, string cacheKey, TimeSpan duration);
+    }
+}
