6b77ad6e251f12fa9e93d580ef75c607c5b33cb2
Christopher House <chris.house@ecolab.com>
2018-12-18 13:26:36 -0600

    added tests for order controller

---
+++ Tests/ThirteenDaysAWeek.iFlyShop.Api.UnitTests/Controllers/OrderControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using ThirteenDaysAWeek.iFlyShop.Api.Controllers;
+using ThirteenDaysAWeek.iFlyShop.Api.Services;
+using Xunit;
+
+namespace ThirteenDaysAWeek.iFlyShop.Api.UnitTests.Controllers
+{
+    public class OrderControllerTests
+    {
+        private readonly MockRepository _mockRepository = new MockRepository(MockBehavior.Strict);
+        private readonly Mock<IOrderPublisher> _mockOrderPublisher;
+
+        public OrderControllerTests()
+        {
+            _mockOrderPublisher = _mockRepository.Create<IOrderPublisher>();
+        }
+
+        [Fact]
+        public void Constructor_Should_Throw_ArgumentNullException_When_OrderPublisher_Is_Null()
+        {
+            // Arrange
+            IOrderPublisher publisher = null;
+            Action ctor = () => new OrderController(publisher);
+
+            // Act + Assert
+            ctor.Should()
+                .Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Constructor_Should_Not_Throw_ArgumentNullException_When_OrderPublisher_Is_Not_Null()
+        {
+            // Arrange
+            Action ctor = () => new OrderController(_mockOrderPublisher.Object);
+
+            // Act + Assert
+            ctor.Should()
+                .NotThrow<ArgumentNullException>();
+        }
+    }
+}
