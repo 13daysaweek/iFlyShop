3612f1e517966a3c1558ad8e84c424c1a99f44b9
Christopher House <chris.house@ecolab.com>
2018-12-18 12:06:17 -0600

    wired fluentvalidations into web api

--- Backend/ThirteenDaysAWeek.iFlyShop.Api/App_Start/WebApiConfig.cs
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/App_Start/WebApiConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
+using FluentValidation.WebApi;
 
 namespace ThirteenDaysAWeek.iFlyShop.Api
 {
@@ -19,6 +17,8 @@ namespace ThirteenDaysAWeek.iFlyShop.Api
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            FluentValidationModelValidatorProvider.Configure(config);
         }
     }
 }
