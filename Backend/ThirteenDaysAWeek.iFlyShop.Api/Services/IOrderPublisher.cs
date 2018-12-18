aa2d0d89f4935220d1e59fd15329d31696658211
Christopher House <chris.house@ecolab.com>
2018-12-18 13:09:13 -0600

    implemented order queueing

---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Services/IOrderPublisher.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ThirteenDaysAWeek.iFlyShop.Shared.Models;
+
+namespace ThirteenDaysAWeek.iFlyShop.Api.Services
+{
+    public interface IOrderPublisher
+    {
+        Task SubmitOrderAsync(Order order);
+    }
+}
