b16d60feebeeb0501221dcaddb021325066e98c0
Christopher House <chhouse@microsoft.com>
2018-12-18 10:44:06 -0600

    Finished order line item tests

---
+++ Tests/ThirteenDaysAWeek.iFlyShop.Shared.UnitTests/TheoryProviders/InvalidIntegerProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ThirteenDaysAWeek.iFlyShop.Shared.UnitTests.TheoryProviders
+{
+    public class InvalidIntegerProvider : IEnumerable<object[]>
+    {
+        private static readonly IEnumerable<object[]> _data = new List<object[]>
+        {
+            new []{ (object)0 },
+            new []{ (object)-1 }
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- Tests/ThirteenDaysAWeek.iFlyShop.Shared.UnitTests/Validation/OrderLineItemValidatorTests.cs
+++ Tests/ThirteenDaysAWeek.iFlyShop.Shared.UnitTests/Validation/OrderLineItemValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using ThirteenDaysAWeek.iFlyShop.Shared.Models;
+using ThirteenDaysAWeek.iFlyShop.Shared.UnitTests.TheoryProviders;
 using ThirteenDaysAWeek.iFlyShop.Shared.Validation;
 using Xunit;
 
@@ -22,6 +23,54 @@ namespace ThirteenDaysAWeek.iFlyShop.Shared.UnitTests.Validation
             result.IsValid
                 .Should()
                 .BeTrue();
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidIntegerProvider))]
+        public void Should_Not_Be_Valid_When_ProductId_Is_Zero(int productId)
+        {
+            // Arrange
+            var item = new OrderLineItem {ProductId = productId, Quantity = 1};
+
+            // Act
+            var result = _validator.Validate(item);
+
+            // Assert
+            result.IsValid
+                .Should()
+                .BeFalse();
+
+            result.Errors.Count
+                .Should()
+                .Be(1);
+
+            result.Errors[0].ErrorMessage
+                .Should()
+                .Be(ValidationMessages.OrderLineItemValidator_ProductId_Must_Be_Greater_Than_Zero);
+        }
+
+        [Theory]
+        [ClassData(typeof(InvalidIntegerProvider))]
+        public void Should_Not_Be_Valid_When_Quantity_Is_Zero(int quantity)
+        {
+            // Arrange
+            var item = new OrderLineItem { ProductId = 1, Quantity = quantity };
+
+            // Act
+            var result = _validator.Validate(item);
+
+            // Assert
+            result.IsValid
+                .Should()
+                .BeFalse();
+
+            result.Errors.Count
+                .Should()
+                .Be(1);
+
+            result.Errors[0].ErrorMessage
+                .Should()
+                .Be(ValidationMessages.OrderLineItemValidator_Quantity_Must_Be_Greater_Than_Zero);
         }
     }
 }
