1ade494e03557e79230bd338eec20e5fcacedc3d
Christopher House <chhouse@microsoft.com>
2018-12-18 10:30:46 -0600

    Added test proj for models + validation

---
+++ Tests/ThirteenDaysAWeek.iFlyShop.Shared.UnitTests/Validation/OrderLineItemValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using ThirteenDaysAWeek.iFlyShop.Shared.Models;
+using ThirteenDaysAWeek.iFlyShop.Shared.Validation;
+using Xunit;
+
+namespace ThirteenDaysAWeek.iFlyShop.Shared.UnitTests.Validation
+{
+    public class OrderLineItemValidatorTests
+    {
+        private readonly OrderLineItemValidator _validator = new OrderLineItemValidator();
+
+        [Fact]
+        public void Should_Be_Valid_When_ProductId_And_Quantity_Are_Greater_Than_Zero()
+        {
+            // Arrange
+            var item = new OrderLineItem {ProductId = 1, Quantity = 1};
+
+            // Act
+            var result = _validator.Validate(item);
+
+            // Assert
+            result.IsValid
+                .Should()
+                .BeTrue();
+        }
+    }
+}
