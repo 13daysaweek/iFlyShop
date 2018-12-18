5313c4e8dda8a151aeb9634e48a17c62ae2fee2f
Christopher House <chhouse@microsoft.com>
2018-12-18 09:48:21 -0600

    Created model

---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThirteenDaysAWeek.iFlyShop.Api.Models
+{
+    public class Order
+    {
+        public Order()
+        {
+            Items = new List<OrderLineItem>();
+        }
+
+        public string OrderNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public IList<OrderLineItem> Items { get; }
+    }
+}
---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Models/OrderLineItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ThirteenDaysAWeek.iFlyShop.Api.Models
+{
+    public class OrderLineItem
+    {
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
