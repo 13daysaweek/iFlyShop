f362537fb855725ce1e532a457119d1c94240083
Christopher House <chris.house@ecolab.com>
2018-12-20 08:20:37 -0600

    removed template files, just using .config files from setup script instead

--- LocalDevSetup/local-dev-setup.ps1
+++ LocalDevSetup/local-dev-setup.ps1
@@ -7,19 +7,19 @@
 # Invoke-Expression ".\$scriptPath" 
 
 # Placeholder variables
-$sqlConnStringPlaceholder = "#iFlyShopContextConnString#"
-$cacheConnectionStringPlaceholder = "#cacheConnectionString#"
-$storageAccountPlaceholder = "#storageConnectionString#"
+$sqlConnStringPlaceholder = "__iFlyShopContext__"
+$cacheConnectionStringPlaceholder = "__cacheConnectionString__"
+$storageAccountPlaceholder = "__storageConnectionString__"
 
-$appInsightsiKeyPlaceholder = "#iKey#"
-$orderQueueNamePlaceholder = "#orderQueueName#"
+$appInsightsiKeyPlaceholder = "__iKey__"
+$orderQueueNamePlaceholder = "__orderQueueName__"
 
 # Output folder
 $outputFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $outputFolder = Join-Path $outputFolder '..\Backend\ThirteenDaysAWeek.iFlyShop.Api'
 
 # Template Folder
-$templateFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+#$templateFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
 # Capture SQL DB connection string
 $sqlConnString = Read-Host -Prompt 'Enter the connection string for your SQL database'
@@ -37,19 +37,18 @@ $appInsightsiKey = Read-Host -Prompt 'Enter the Instrumentation Key for your App
 $orderQueueName = Read-Host -Prompt 'Enther the queue name for orders'
 
 # Read connection string template file
-$connStringTemplatePath = Join-Path $templateFolder 'api.connectionStrings.template'
+$connStringTemplatePath = Join-Path $outputFolder 'api.connectionStrings.config'
 $connStringTemplate = Get-Content -Path $connStringTemplatePath
 
 # Replace Placeholders
 $outputContent = $connStringTemplate.Replace($sqlConnStringPlaceholder, $sqlconnString).Replace($cacheConnectionStringPlaceholder, $cacheConnectionString).Replace($storageAccountPlaceholder, $storageAccountConnectionString)
 
 # Create file in output folder with updated template string
-$connStringOutputPath = Split-Path $MyInvocation.MyCommand.Definition -Parent
-$connStringOutputPath = Join-Path $connStringOutputPath '..\Backend\ThirteenDaysAWeek.iFlyShop.Api\api.connectionStrings.local'
+$connStringOutputPath = Join-Path $outputFolder 'api.connectionStrings.local'
 Out-File -FilePath $connStringOutputPath -InputObject $outputContent
 
 # Copy appSettings template to output folder (no values in this file yet, so just a straight copy)
-$appSettingsTemplate = Join-Path $templateFolder 'api.appSettings.template'
+$appSettingsTemplate = Join-Path $outputFolder 'api.appSettings.config'
 $appSettingsOutputFile = Join-Path $outputFolder 'api.appSettings.local'
 $appSettingsContent = Get-Content -Path $appSettingsTemplate
 
