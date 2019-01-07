b3a02d26e1d7772fbc5aa947d83904135e3289db
Christopher House <chris.house@ecolab.com>
2019-01-07 13:01:48 -0600

    implemented app insights stuff

--- Frontend/iflyshop-ui/src/services/monitoring.service.ts
+++ Frontend/iflyshop-ui/src/services/monitoring.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AppInsights } from 'applicationinsights-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonitoringService {
+  private config: Microsoft.ApplicationInsights.IConfig = {
 
-  constructor() { }
+  };
+
+  logPageView(name?: string,
+    url?: string,
+    properties?: any,
+    measurements?: any,
+    duration?: number) {
+      AppInsights.trackPageView(name, url, properties, measurements, duration);
+    }
+
+  logEvent(name: string, properties?: any, measurements?: any) {
+    AppInsights.trackEvent(name, properties, measurements);
+  }
+
+  logTrace(message: string, properties?: any, severityLevel?: any) {
+    AppInsights.trackTrace(message, properties, severityLevel);
+  }
+
+  logException(e: any) {
+    AppInsights.trackException(e);
+  }
+
+  constructor() {
+    if (!AppInsights.config) {
+      AppInsights.downloadAndSetup(this.config);
+    }
+  }
 }
