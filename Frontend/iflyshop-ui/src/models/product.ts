0caa12b2b8ae291d9fb7085fdd7b0dd9dda7339e
Christopher House <chris.house@ecolab.com>
2019-01-07 10:20:08 -0600

    added product model

---
+++ Frontend/iflyshop-ui/src/models/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    productId: number;
+    name: string;
+    unitPrice: number;
+    cost: number;
+    productThumbnailImagePath: string;
+    dateModified: Date;
+    dateCreated: Date;
+}
