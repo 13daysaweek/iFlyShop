cdbb6097efe3523784020099dfaceb3ac544eda5
Christopher House <chris.house@ecolab.com>
2019-01-07 13:21:06 -0600

    added config service

--- Frontend/iflyshop-ui/src/services/app-config-service.service.ts
+++ Frontend/iflyshop-ui/src/services/app-config-service.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppConfigServiceService {
+  private appConfig;
 
-  constructor() { }
+  loadAppConfig() {
+    const http = this.injector.get(HttpClient);
+
+    return http.get('/assets/app-config.json')
+      .toPromise()
+      .then(data => {
+        this.appConfig = data;
+      });
+  }
+
+  get config() {
+    return this.appConfig;
+  }
+
+  constructor(private injector: Injector) { }
 }
