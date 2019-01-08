f43c3697637da08e5758876061a17cbd7f4f10f5
Christopher House <chris.house@ecolab.com>
2019-01-08 11:22:56 -0600

    first pass at shopping cart

--- Frontend/iflyshop-ui/src/app/product/product.component.ts
+++ Frontend/iflyshop-ui/src/app/product/product.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { MonitoringService } from '../../services/monitoring.service';
+import { Product } from '../../models/product';
+import { CartItem } from '../../models/cartItem';
+import { ShoppingCart } from '../../models/shoppingCart';
 
 @Component({
   selector: 'app-product',
@@ -6,8 +11,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  @Input() product: Product;
 
-  constructor() { }
+  quantity: number;
+
+  addItemToCart(): void {
+    const cartKey = 'cart';
+    let cartString = localStorage.getItem(cartKey);
+    let cart: ShoppingCart;
+
+    const item = new CartItem();
+    item.productId = this.product.productId;
+    item.quantity = this.quantity;
+    item.unitPrice = this.product.unitPrice;
+
+    if (cartString === null) {
+      cart = new ShoppingCart();
+      cart.items.push(item);
+    } else {
+      cart = JSON.parse(cartString);
+      cart.items.push(item);
+    }
+
+    cartString = JSON.stringify(cart);
+    localStorage.setItem(cartKey, cartString);
+  }
+
+  constructor(private monitoringService: MonitoringService) { }
 
   ngOnInit() {
   }
---
+++ Frontend/iflyshop-ui/src/models/shoppingCart.ts
@@ -0,0 +1,15 @@
+import { CartItem } from './cartItem';
+
+export class ShoppingCart {
+    items: CartItem[];
+
+    getTotal(): number {
+        const total = this.items.map(_ => _.quantity * _.unitPrice).reduce((p, n) => p + n);
+
+        return total;
+    }
+
+    constructor() {
+        this.items = [];
+    }
+}
