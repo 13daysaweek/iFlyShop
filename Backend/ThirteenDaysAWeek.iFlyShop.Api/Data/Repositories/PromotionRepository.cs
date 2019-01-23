5f6938914bcfd44f4c3b8dd8df5a74429cce7926
Christopher House <chris.house@ecolab.com>
2019-01-23 08:54:18 -0600

    DB migration + repo

---
+++ Backend/ThirteenDaysAWeek.iFlyShop.Api/Data/Repositories/PromotionRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using ThirteenDaysAWeek.iFlyShop.Api.Models;
+
+namespace ThirteenDaysAWeek.iFlyShop.Api.Data.Repositories
+{
+    public class PromotionRepository : IRepository<Promotion>
+    {
+        private readonly IContextFactory _contextFactory;
+
+        public PromotionRepository(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+        }
+
+        public async Task<IEnumerable<Promotion>> GetAll()
+        {
+            var promotions = default(IEnumerable<Promotion>);
+
+            using (var context = _contextFactory.CreateInstance())
+            {
+                promotions = await context.Promotions.ToListAsync();
+            }
+
+            return promotions;
+        }
+
+        public async Task<Promotion> GetById(int id)
+        {
+            var promotion = default(Promotion);
+
+            using (var context = _contextFactory.CreateInstance())
+            {
+                promotion = await context.Promotions.FirstAsync(_ => _.PromotionId == id);
+            }
+
+            return promotion;
+        }
+    }
+}
